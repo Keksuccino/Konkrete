462fe222f0a51688b03c5d0918746da4ccfdcedb
Keksuccino <35544624+Keksuccino@users.noreply.github.com>
2022-01-04 18:25:56 +0100

    Release v1.3.3

---
+++ src/main/java/de/keksuccino/konkrete/Test.java
@@ -0,0 +1,18 @@
+package de.keksuccino.konkrete;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import de.keksuccino.konkrete.events.SubscribeEvent;
+import de.keksuccino.konkrete.events.client.ClientTickEvent;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawableHelper;
+
+public class Test {
+	
+//	@SubscribeEvent
+//	public void onDrawPre(ClientTickEvent e) {
+//		System.out.println("DRAW PRE!");
+//		RenderSystem.enableBlend();
+//		DrawableHelper.drawCenteredText(e.getMatrixStack(), MinecraftClient.getInstance().textRenderer, "Huhuuuu", 20, 20, -1);
+//	}
+	
+}
