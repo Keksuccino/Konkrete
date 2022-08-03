e848ff413f9fda03ae6dde5a45bdb74299438321
Keksuccino <35544624+Keksuccino@users.noreply.github.com>
2022-08-03 18:16:49 +0200

    Release v1.5.0

--- src/main/java/de/keksuccino/konkrete/Konkrete.java
+++ src/main/java/de/keksuccino/konkrete/Konkrete.java
@@ -24,7 +24,7 @@ import org.apache.logging.log4j.Logger;
 
 public class Konkrete implements ModInitializer {
 
-	public static final String VERSION = "1.4.0";
+	public static final String VERSION = "1.5.0";
 
     private static final EventHandler HANDLER = new EventHandler();
 
@@ -57,8 +57,6 @@ public class Konkrete implements ModInitializer {
 				LOGGER.info("[KONKRETE] Optifine detected! ###############################");
 			}
 			catch (ClassNotFoundException e) {}
-
-//			HANDLER.registerEventsFrom(new TestEvents());
 		
 		}
 
