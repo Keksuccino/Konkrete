4b20749daca58a792cb12579887db0be8d86b566
Keksuccino <35544624+Keksuccino@users.noreply.github.com>
2022-12-20 23:36:12 +0100

    Release v1.6.0

--- src/main/java/de/keksuccino/konkrete/Konkrete.java
+++ src/main/java/de/keksuccino/konkrete/Konkrete.java
@@ -24,7 +24,7 @@ import org.apache.logging.log4j.Logger;
 
 public class Konkrete implements ModInitializer {
 
-	public static final String VERSION = "1.5.3";
+	public static final String VERSION = "1.6.0";
 
     private static final EventHandler HANDLER = new EventHandler();
 
