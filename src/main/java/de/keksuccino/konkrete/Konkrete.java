10894f458be789d35ec97e855b6bcd71655ad2dd
Keksuccino <35544624+Keksuccino@users.noreply.github.com>
2024-01-05 11:02:28 +0100

    Ported to MC 1.20.4

--- src/main/java/de/keksuccino/konkrete/Konkrete.java
+++ src/main/java/de/keksuccino/konkrete/Konkrete.java
@@ -21,7 +21,7 @@ import org.apache.logging.log4j.Logger;
 @Mod("konkrete")
 public class Konkrete {
 
-	public static final String VERSION = "1.6.1";
+	public static final String VERSION = "1.7.1";
 
 	public static Logger LOGGER = LogManager.getLogger();
 
