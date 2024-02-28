34844d4c7779230bca5a1affc2d663b385e12b18
Keksuccino <35544624+Keksuccino@users.noreply.github.com>
2024-02-28 08:25:02 +0100

    Update Konkrete.java

--- src/main/java/de/keksuccino/konkrete/Konkrete.java
+++ src/main/java/de/keksuccino/konkrete/Konkrete.java
@@ -22,7 +22,7 @@ import org.apache.logging.log4j.Logger;
 
 public class Konkrete implements ModInitializer {
 
-	public static final String VERSION = "1.8.0";
+	public static final String VERSION = "1.8.1";
 
     private static final EventHandler HANDLER = new EventHandler();
 
