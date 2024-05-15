d7ec74e5d1d36a8204e51641208d9b9690c37138
Keksuccino <35544624+Keksuccino@users.noreply.github.com>
2024-05-15 12:47:57 +0200

    build.gradle, Konkrete.java und 3 weitere dateien aktualisiert...

---
+++ forge/src/main/java/de/keksuccino/konkrete/GradleVariables.java
@@ -0,0 +1,11 @@
+
+                
+            package de.keksuccino.konkrete;
+
+            public class GradleVariables {
+
+                public static final String MOD_VERSION = "1.9.7";
+
+            }
+            
+            
