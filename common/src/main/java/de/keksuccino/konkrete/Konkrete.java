1f34f4433ffffbae7602ca9bd7dd5b851941a91d
Keksuccino <35544624+Keksuccino@users.noreply.github.com>
2024-06-15 12:48:38 +0200

    ---

--- common/src/main/java/de/keksuccino/konkrete/Konkrete.java
+++ common/src/main/java/de/keksuccino/konkrete/Konkrete.java
@@ -15,7 +15,7 @@ public class Konkrete {
 	private static final Logger LOGGER = LogManager.getLogger();
 
 	public static final String MOD_ID = "konkrete";
-	public static final String VERSION = "1.9.8";
+	public static final String VERSION = "1.9.9";
 	public static final String MOD_LOADER = Services.PLATFORM.getPlatformName();
 
 	@Deprecated(forRemoval = true)
