a08d9d7cd933dd40a3c6a13e54aca3bb0b8121fd
Commit Bot - Keksuccino <commit_bot@keksuccino.com>
2025-10-15 05:38:04 +0200

    v1.9.13

--- forge/src/main/java/de/keksuccino/konkrete/platform/ForgeCompatibilityLayer.java
+++
@@ -1,7 +0,0 @@
-package de.keksuccino.konkrete.platform;
-
-import de.keksuccino.konkrete.platform.services.IPlatformCompatibilityLayer;
-
-public class ForgeCompatibilityLayer implements IPlatformCompatibilityLayer {
-
-}
