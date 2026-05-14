ce616ae4049b1f24a5441818197a938fe5bab81d
Keksuccino <no@mail.net>
2026-05-14 21:43:22 +0200

    v1.10.2

--- common/src/main/java/de/keksuccino/konkrete/Konkrete.java
+++ common/src/main/java/de/keksuccino/konkrete/Konkrete.java
@@ -30,7 +30,7 @@ public class Konkrete {
 	private static final Logger LOGGER = LogManager.getLogger();
 
 	public static final String MOD_ID = "konkrete";
-	public static final String VERSION = "1.10.1";
+	public static final String VERSION = "1.10.2";
 	public static final String MOD_LOADER = Services.PLATFORM.getPlatformName();
 
 	@Deprecated
